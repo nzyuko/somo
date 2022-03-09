01623e8df2cdcfcc42a6faeb6e8aa71d3ceb343a
nzyuko <johnnzyuko514@gmail.com>
2022-03-09 20:10:07 +0300

    added navbar template and home and room skeleton templates

--- somo-back/somo/base/views.py
+++ somo-back/somo/base/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return HttpResponse('Homepage')
+    return render(request, 'home.html')
+
 
 def room(request):
-    return HttpResponse('ROOM')
+    return render(request, 'room.html')
--- somo-back/somo/somo/settings.py
+++ somo-back/somo/somo/settings.py
@@ -44,7 +44,9 @@ ROOT_URLCONF = 'somo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
