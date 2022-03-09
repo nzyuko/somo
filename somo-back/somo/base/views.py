b4924ef1a86d94fb3ae1e6236e58a8b49468022d
nzyuko <johnnzyuko514@gmail.com>
2022-03-09 21:23:53 +0300

    added navbar template and home and room skeleton templates

--- somo-back/somo/base/views.py
+++ somo-back/somo/base/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name': 'lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend Developers'},
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 
 def room(request):
