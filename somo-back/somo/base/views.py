a1e26098d0008123f54f5b86228b09f004e54b4e
nzyuko <johnnzyuko514@gmail.com>
2022-03-12 01:16:42 +0300

    added delete room feature

--- somo-back/somo/base/views.py
+++ somo-back/somo/base/views.py
@@ -36,3 +36,11 @@ def updateRoom(request, pk):
             return redirect('home')
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'room': room})
