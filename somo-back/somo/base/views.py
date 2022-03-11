57f990c6ddd79295f662073be8cf311be8a31f1a
nzyuko <johnnzyuko514@gmail.com>
2022-03-11 15:45:36 +0300

    added fields in models.py restructred and moved templates to base folder from the root folder created  room_form html for customized functionality

--- somo-back/somo/base/views.py
+++ somo-back/somo/base/views.py
@@ -12,10 +12,14 @@ from .models import Room
 def home(request):
     rooms = Room.objects.all()
     context = {'rooms': rooms}
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
-    return render(request, 'room.html', context)
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
