f3066dee9b197b601f7eae91e630585473429e49
nzyuko <johnnzyuko514@gmail.com>
2022-03-15 14:28:25 +0300

    added login and its corresponding function in views.py included login page link in navbar

--- somo-back/somo/base/views.py
+++ somo-back/somo/base/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render, redirect
-
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
 
 # To fix Q only seems to work for topic__name
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password is incorrect')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 
 def home(request):
