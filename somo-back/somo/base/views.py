ad23775d7dff693c1627322b4f1ed3c760c888c1
nzyuko <johnnzyuko514@gmail.com>
2022-03-16 15:31:16 +0300

    added restrictions. user must be logged in to make changes to a room

--- somo-back/somo/base/views.py
+++ somo-back/somo/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
@@ -57,6 +58,7 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
+@login_required(login_url='/login/')
 def createRoom(request):
     form = RoomForm
     if request.method == 'POST':
@@ -68,6 +70,7 @@ def createRoom(request):
     return render(request, 'base/room_form.html', context)
 
 
+@login_required(login_url='/login/')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
@@ -80,6 +83,7 @@ def updateRoom(request, pk):
     return render(request, 'base/room_form.html', context)
 
 
+@login_required(login_url='/login/')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
     if request.method == 'POST':
