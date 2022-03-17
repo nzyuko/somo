73a2ad35dbac58a850eaf6a200a1ab2dc0c5948f
nzyuko <johnnzyuko514@gmail.com>
2022-03-17 11:22:15 +0300

    Added Register User functionality. added register user url to base/urls created register user view added render logic to handle new users in base/login_register.html

--- somo-back/somo/base/views.py
+++ somo-back/somo/base/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
@@ -12,7 +13,7 @@ from .forms import RoomForm
 # To fix Q only seems to work for topic__name
 
 def loginPage(request):
-
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
 
@@ -33,13 +34,18 @@ def loginPage(request):
         else:
             messages.error(request, 'Username or Password is incorrect')
 
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerPage(request):
+    form = UserCreationForm()
+    return render(request, 'base/login_register.html', {'form': form})
 
 
 def home(request):
