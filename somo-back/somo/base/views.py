05217660bf9191afcc32395504d5e922faffa0b7
nzyuko <johnnzyuko514@gmail.com>
2022-03-17 12:41:35 +0300

    mapped the commit message form in room.html onto its corresponding model attribute

--- somo-back/somo/base/views.py
+++ somo-back/somo/base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 
@@ -77,6 +77,15 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+
+        )
+        return redirect('room', pk=room.id)
     context = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html', context)
 
