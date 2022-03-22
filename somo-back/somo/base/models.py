3ac5734d9ed0b192de9f6b53cfa68f6a6e810e68
nzyuko <johnnzyuko514@gmail.com>
2022-03-22 09:39:57 +0300

    added participants to Room model

--- somo-back/somo/base/models.py
+++ somo-back/somo/base/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 # Create your models here.
@@ -16,7 +16,7 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=250)
     description = models.TextField(null=True, blank=True)
-    # participants =
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -36,5 +36,3 @@ class Message(models.Model):
 
     def __str__(self):
         return self.body[0:50]
-
-
