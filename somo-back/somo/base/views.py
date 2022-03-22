17636c24ffc2bebec55abdca0663eb0d66034133
nzyuko <johnnzyuko514@gmail.com>
2022-03-22 09:50:39 +0300

    added participants in room view and its context dictionary

--- somo-back/somo/base/views.py
+++ somo-back/somo/base/views.py
@@ -1,13 +1,14 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
-from django.db.models import Q
-from .models import Room, Topic, Message
+
 from .forms import RoomForm
+from .models import Room, Topic, Message
 
 
 # To fix Q only seems to work for topic__name
@@ -77,6 +78,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -86,7 +88,7 @@ def room(request, pk):
 
         )
         return redirect('room', pk=room.id)
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 
