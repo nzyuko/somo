b2e50568468076d087d452d594a195ca4e76a296
nzyuko <johnnzyuko514@gmail.com>
2022-03-23 15:42:09 +0300

    delete message url

--- somo-back/somo/base/urls.py
+++ somo-back/somo/base/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
@@ -11,5 +12,6 @@ urlpatterns = [
 
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
-    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room")
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>', views.deleteMessage, name="delete-message"),
 ]
