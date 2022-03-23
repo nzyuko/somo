c2765a93c98c6f97726e190df957920b4dece8fe
nzyuko <johnnzyuko514@gmail.com>
2022-03-23 15:20:23 +0300

    rendered participants on the sidebar in room.html

--- somo-back/somo/base/models.py
+++ somo-back/somo/base/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 
-class Topic (models.Model):
+class Topic(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
