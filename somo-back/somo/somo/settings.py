116884b845a64dce201f185168df72e4c645cf28
nzyuko <johnnzyuko514@gmail.com>
2022-03-24 21:12:43 +0300

    added static folder in root and specified settings.py

--- somo-back/somo/somo/settings.py
+++ somo-back/somo/somo/settings.py
@@ -110,6 +110,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# STATIC_ROOT =
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
